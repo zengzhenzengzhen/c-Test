20c85a90f837887e10592cf9bf66e9f125512593
zz <1565284370@qq.com>
2019-07-01 15:52:02 +0800

    倒计时

---
+++ c#示例/TimerTest/TimerTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerTest
+{
+    class Program
+    {
+        public static int m = 1;
+        public static int s = 60;
+        public static System.Timers.Timer timer = new System.Timers.Timer();
+        static void Main(string[] args)
+        {
+            
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(djs);
+            timer.Start();
+            
+            Console.ReadKey();
+        }
+
+        private static void djs(object source,ElapsedEventArgs e)
+        {
+
+            s--;
+            if (s < 0)
+            {
+                s = 59;
+                m--;
+            }
+            var min = m;
+            var sec = s;
+            if(m==0 && s==0)
+            {
+                timer.Stop();
+            }
+            Console.WriteLine(m + "分" + s + "秒");
+        }
+        
+    }
+}
